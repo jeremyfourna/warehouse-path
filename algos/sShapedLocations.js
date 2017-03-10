f7d52bb46e59a0581067aaef6e53779adc581cfd
Jeremy Fourna <jeremy.fourna@westwing.de>
2017-03-10 12:25:13 +0100

    Rename a function and add a reverse one

--- algos/sShapedLocations.js
+++ algos/sShapedLocations.js
@@ -1,10 +1,25 @@
-// sShapedLocation :: [String] -> [String];
-function sShapedLocation(locationsList) {
+// sShapedLocationAsc :: [String] -> [String];
+function sShapedLocationAsc(locationsList) {
 	let newList = locationsList.slice(0);
 	newList.sort(function(a, b) {
 		if (a < b) {
 			return -1;
 		} else if (a > b) {
+			return 1;
+		} else {
+			return 0;
+		}
+	});
+	return newList;
+}
+
+// sShapedLocationDesc :: [String] -> [String];
+function sShapedLocationDesc(locationsList) {
+	let newList = locationsList.slice(0);
+	newList.sort(function(a, b) {
+		if (a > b) {
+			return -1;
+		} else if (a < b) {
 			return 1;
 		} else {
 			return 0;
