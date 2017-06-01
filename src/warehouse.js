22530fe8b4040fbf7911176616d5006bb5eaec44
Jeremy Fourna <jeremy.fourna@westwing.de>
2017-06-01 17:15:13 +0200

    Almost finish refactoring of warehouse.js

--- src/warehouse.js
+++ src/warehouse.js
@@ -2,67 +2,50 @@ const R = require('ramda')
 
 
 exports.aislesWithWalls = aislesWithWalls
+exports.checkListOfSeparation = checkListOfSeparation
 exports.createMatrix = createMatrix
+exports.createWarehouseMatrix = createWarehouseMatrix
 exports.isNegativeValuesInArray = isNegativeValuesInArray
 exports.racksFaced = racksFaced
 
 
+const isNaN = R.equals(NaN)
+
+
 /**
- * Matrix = [ [Number], [Number] ] 
+ * Matrix = [ [Number], [Number] ]
+ *
+ * A matrix looks like this : 0 is a walkable location  1 is a wall (not walkable)
+ * [
+ *		[0, 0, 0, 0, 0],
+ *		[0, 0, 1, 0, 0],
+ *		[0, 0, 1, 0, 0],
+ *		[0, 0, 0, 0, 0]
+ * ]
  */
 
 // createWarehouseMatrix :: (Number -> Number -> [Number]) -> Matrix
-function createWarehouseMatrix(numberOfRackInAisle, numberOfAisleInWarehouse, listOfSeparation) {
-	// Check parameters
-	if (numberOfRackInAisle <= 0) {
-		console.log(`numberOfRackInAisle should be > 0  the value passed was ${numberOfRackInAisle}`)
-		return []
-	} else if (numberOfRackInAisle % 2 !== 0) {
-		console.log(`numberOfRackInAisle should be even  the value passed was ${numberOfRackInAisle}`)
-		return []
-	} else if (numberOfAisleInWarehouse <= 0) {
-		console.log(`numberOfAisleInWarehouse should be > 0  the value passed was ${numberOfAisleInWarehouse}`)
-		return []
-	} else if (listOfSeparation.length === undefined) {
-		console.log(`listOfSeparation should be an Array of positive numbers  the value passed was ${listOfSeparation}`)
-		return []
-	} else if (isNegativeValuesInArray(listOfSeparation).length > 0) {
-		console.log(`There should be only numbers >= 0 inside listOfSeparation  we found those numbers inside  ${isNegativeValuesInArray(listOfSeparation)}`)
-		return []
-	}
-
-
-	// A matrix looks like this : 0 is a walkable location  1 is a wall (not walkable)
-	/* [
-		[0, 0, 0, 0, 0],
-		[0, 0, 1, 0, 0],
-		[0, 0, 1, 0, 0],
-		[0, 0, 0, 0, 0]
-	] */
-	return createMatrix(numberOfRackInAisle, aislesWithWalls(numberOfAisleInWarehouse), listOfSeparation)
+function createWarehouseMatrix(racksInAisle, aislesInWH, listOfSeparation) {
+	return createMatrix(racksFaced(racksInAisle), aislesWithWalls(aislesInWH), checkListOfSeparation(listOfSeparation))
 }
 
 // createMatrix :: Number -> Number -> [Number] -> Number -> Number -> Array -> [Array]
 function createMatrix(racksInAisle, aislesInWH, listOfSeparation, height = 0, width = 0, matrix = []) {
 	// Walkability matrix. Zero is walkable, One is not
-	if (height >= racksInAisle) {
+	if (R.gt(height, racksInAisle)) {
 		matrix.push(Array(aislesInWH).fill(0))
 		return matrix
-	} else if (width >= aislesInWH) {
+	} else if (R.gte(width, aislesInWH)) {
 		return createMatrix(racksInAisle, aislesInWH, listOfSeparation, height + 1, 0, matrix)
 	} else {
-		if (height === 0) {
+		if (R.equals(height, 0)) {
 			matrix.push(Array(aislesInWH).fill(0))
 			return createMatrix(racksInAisle, aislesInWH, listOfSeparation, height + 1, width, matrix)
-		} else if (matrix[height] === undefined) {
+		} else if (R.isNil(R.nth(height, matrix))) {
 			matrix.push([])
 			return createMatrix(racksInAisle, aislesInWH, listOfSeparation, height, width, matrix)
 		} else {
-			if (listOfSeparation.find(function(cur) {
-					return cur === height
-				}) !== undefined) {
-				matrix[height].push(0)
-			} else if ((width + 1) % 3 === 0) {
+			if (R.equals(R.modulo(R.inc(width), 3), 0)) {
 				matrix[height].push(1)
 			} else {
 				matrix[height].push(0)
@@ -78,9 +61,8 @@ function aislesWithWalls(aislesNumber) {
 	//   1   |   2   |   3   |   4   |   5   |   n
 	// a : a | a : a | a : a | . : . | . : . | n : n
 	const safeAislesNumber = Math.round(aislesNumber)
-	const isNaN = R.equals(NaN)
 	if (R.either(isNaN, R.lt(R.__, 1))(safeAislesNumber)) {
-		throw new Error(`aislesNumber must be a positive integer but aislesNumber was rounded to ${aislesNumber}`)
+		throw new Error(`aislesWithWalls : parameter must be a positive integer but parameter was rounded to ${aislesNumber}`)
 	} else {
 		return R.dec(R.multiply(safeAislesNumber, 3))
 	}
@@ -98,16 +80,23 @@ function racksFaced(racksNumber) {
 	//
 	// So we need to divide the numberOfRackInAisle by two to respect the real warehouse height
 	const safeRacksNumber = Math.round(racksNumber)
-	const isNaN = R.equals(NaN)
 	const isOdd = R.ifElse(R.modulo(R.__, 2), R.always(true), R.always(false))
-	if (R.anyPass([isNaN, R.lte(R.__, 2), isOdd])(safeRacksNumber)) {
-		throw new Error(`racksNumber must be an even positive integer superior to 0 but racksNumber was rounded to ${racksNumber}`)
+	if (R.anyPass([isNaN, R.lt(R.__, 2), isOdd])(safeRacksNumber)) {
+		throw new Error(`racksFaced : parameter must be an even positive integer superior to 0 but parameter was rounded to ${racksNumber}`)
 	} else {
 		return R.divide(safeRacksNumber, 2)
 	}
 }
 
-// isNegativeValuesInArray :: Array -> Array
+// isNegativeValuesInArray :: [Number] -> [Number]
 function isNegativeValuesInArray(list) {
 	return R.filter(R.lt(R.__, 0), list)
+}
+
+function checkListOfSeparation(list) {
+	if (R.isNil(list) || R.length(isNegativeValuesInArray(list)) > 0) {
+		throw new Error(`checkListOfSeparation : parameter must be an Array of positive numbers but parameter was ${list}`)
+	} else {
+		return list;
+	}
 }
